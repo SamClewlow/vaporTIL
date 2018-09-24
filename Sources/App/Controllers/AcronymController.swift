74765bd4233a1d7a2da586da3f147aeacd51a772
Sam Clewlow <sclewlow@and.digital>
2018-09-24 17:37:56 +0100

    End of lesson 10

---
+++ Sources/App/Controllers/AcronymController.swift
@@ -0,0 +1,43 @@
+import Vapor
+
+struct AcronymsController: RouteCollection {
+    func boot(router: Router) throws {
+        let acronymsRoute = router.grouped("api", "acronyms")
+        acronymsRoute.get(use: getAllHandler)
+        acronymsRoute.post(use: createHandler)
+        acronymsRoute.get(Acronym.parameter, use: getHandler)
+        acronymsRoute.delete(Acronym.parameter, use: deleteHandler)
+        acronymsRoute.put(Acronym.parameter, use: updateHandler)
+    }
+    
+    func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
+        return Acronym.query(on: req).all()
+    }
+    
+    func getHandler(_ req: Request) throws -> Future<Acronym> {
+        return try req.parameters.next(Acronym.self)
+    }
+    
+    func createHandler(_ req: Request) throws -> Future<Acronym> {
+        let acronym = try req.content.decode(Acronym.self)
+        return acronym.save(on: req)
+    }
+    
+    func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req.parameters.next(Acronym.self).flatMap(to: HTTPStatus.self, { (acronym) -> EventLoopFuture<HTTPStatus> in
+            return acronym.delete(on: req).transform(to: .noContent)
+        })
+    }
+    
+    func updateHandler(_ req: Request) throws -> Future<Acronym> {
+        return try flatMap(to: Acronym.self,
+                           req.parameters.next(Acronym.self),
+                           req.content.decode(Acronym.self)) { acronym, updatedAcronym -> Future<Acronym> in
+            acronym.short = updatedAcronym.short
+            acronym.long = updatedAcronym.long
+            return acronym.save(on: req)
+        }
+    }
+}
+
+extension Acronym: Parameter {}
